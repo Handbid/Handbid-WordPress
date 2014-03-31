84356ce1f26669ac2b33897924a583e78b422e07
git <jonhemstreet@gmail.com>
2014-03-31 10:58:37 -0600

    handbid bid plugins javascript functionality

--- public/js/handbid.js
+++ public/js/handbid.js
@@ -1,15 +1,66 @@
-function onDomReady() {
+var $j = jQuery.noConflict();
 
-}
-function onItemUpdate() {
+$j(function() {
 
-}
-function onAuctionUpdated() {
+    var bidAmountDom = document.getElementById("bidAmountTotal");
+    var basePrice = bidAmountDom.innerHTML;
 
-}
-function updateDomForItem() {
+    var bidLabel = document.getElementById("bidLabel");
 
-}
-function updateDomForAuction() {
+    var bidAmount = parseInt($j(".bidDescription .amount .value").text());
+    var bidIncrement = parseInt($j(".bidDescription .bidIncrement .value").text());
 
-}
+    var bidDown = $j( ".bidUpDown .down" );
+    bidDown.on('click', function() {
+        if(bidAmount > basePrice)
+        {
+            bidAmount -= bidIncrement;
+            bidAmountDom.innerHTML = bidAmount;
+            bidLabel.innerHTML = "$" + (bidAmount - basePrice) + " above minimum bid";
+
+            if(bidAmount == basePrice) {
+                bidLabel.innerHTML = "Minimum next bid";
+            }
+        }
+        else
+        {
+            alert('You can not bid lower than the minimum bid')
+        }
+
+    });
+
+    var bidUp = $j( ".bidUpDown .up" );
+    bidUp.on('click', function() {
+        bidAmount += bidIncrement;
+        bidAmountDom.innerHTML = bidAmount;
+        bidLabel.innerHTML = "$" + (bidAmount - basePrice) + " above minimum bid";
+
+    });
+
+    var bidNow = $j( ".bidNow" );
+    bidNow.on('click', function() {
+        alert('Bid submitted at ' + bidAmount);
+        onItemUpdate();
+    });
+
+    var reset = $j( ".bidContainer .reset" );
+    reset.on('click', function() {
+        bidAmount = parseInt(basePrice);
+        bidAmountDom.innerHTML = basePrice;
+        bidLabel.innerHTML = "Minimum next bid";
+    });
+
+    function onItemUpdate() {
+        console.log('onItemUpdate');
+    }
+    function onAuctionUpdated() {
+        console.log('onAuctionUpdated');
+    }
+    function updateDomForItem() {
+        console.log('updateDomForItem');
+    }
+    function updateDomForAuction() {
+        console.log('updateDomForAuction');
+    }
+
+});
