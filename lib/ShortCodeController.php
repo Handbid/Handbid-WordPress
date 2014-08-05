12b76f8660353d7648b94658603564c0f74da2d1
git <jonhemstreet@gmail.com>
2014-08-04 21:46:23 -0400

    shortcode controller update push

--- lib/ShortCodeController.php
+++ lib/ShortCodeController.php
@@ -171,7 +171,6 @@ class ShortCodeController
 
             $template = $this->templateFromAttributes($attributes, 'views/auction/logo');
 
-
             if (!isset($attributes['type']) || !is_array($attributes) || !in_array(
                     $attributes['type'],
                     ['upcoming', 'all', 'past']
