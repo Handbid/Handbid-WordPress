8c69bd78619db49a5067b0f62b0505688aebd3e6
git <jonhemstreet@gmail.com>
2014-08-11 16:19:01 -0600

    banner filter bar removal

--- lib/ShortCodeController.php
+++ lib/ShortCodeController.php
@@ -203,7 +203,6 @@ class ShortCodeController
             $auction = $this->state->currentAuction($attributes);
             $coords  = $auction->location->coords;
 
-            $items          = $this->handbid->store('Item')->byAuction($auction->_id);
             $profile        = null;
             $totalProxies   = null;
             $totalWinning   = null;
@@ -227,25 +226,6 @@ class ShortCodeController
 
             }
 
-            $donorsDirty     = [];
-            $categoriesDirty = [
-                '_all' => 'All'
-            ];
-
-            forEach ($items as $item) {
-
-                if (isset($item->donor)) {
-                    $donorsDirty[] = $item->donor;
-                }
-
-                if (isset($item->term)) {
-                    $categoriesDirty[] = $item->term;
-                }
-
-            }
-
-            $donors     = array_unique($donorsDirty);
-            $categories = array_unique($categoriesDirty);
             $template   = $this->templateFromAttributes($attributes, 'views/auction/banner');
 
             return $this->viewRenderer->render(
@@ -253,8 +233,6 @@ class ShortCodeController
                 [
                     'auction'       => $auction,
                     'coordinates'   => $coords,
-                    'categories'    => $categories,
-                    'donors'        => $donors,
                     'winningBids'   => $totalWinning,
                     'losingBids'    => $totalLosing,
                     'purchases'     => $totalPurchases,
