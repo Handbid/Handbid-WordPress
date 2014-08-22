f76774d01bc8f6311e4a93b12536fe4d2ab54b5b
git <jonhemstreet@gmail.com>
2014-08-22 15:12:42 -0600

    documentation

--- README.md
+++ README.md
@@ -1,13 +1,25 @@
 ![alt tag](https://raw.githubusercontent.com/Handbid/Handbid-WordPress/master/docs/images/handbid.png "Handbid")
 
 Wordpress Plugin
-==
+====
 
 **This plugin is a work in progress**.
 
 This plugin will give you everything you need to turn your Wordpress install into a full fledged silent auction, fund raising machine!
 
-
 ## Setup
-Install the plugin, you will then see a Handbid option in the admin area on the left sidebar. In here is where you can insert your Consumer Key / Consumer Secret
-as well as specify the Rest API Url ( defaults to https://rest.handbid.com ). After you put in your values you can then save the changes and test your rest endpoint credentials to ensure that it is configured properly.
+
+Clone this repo into you plugins folder (wp-content/plugins)
+    - git clone git@github.com:Handbid/Handbid-WordPress.git handbid
+
+Cd into the newly cloned handbid folder and init and update the submodule
+    - git submodule init
+    - git submodule update
+
+Now that we have the plugin cloned and ready to go lets hop into the admin area and install it (/wp-admin/plugins.php)
+
+Let's now hop into the handbid admin dashboard to configure our plugin, you can do so by either clicking the handbid link
+in the menu bar or by going to (/wp-admin/admin.php?page=handbid-admin-dashboard).
+    - Default Auction key will be used on any auction related shortcodes if a auctionkey is not provided as an attribute
+    - Default Organization key will be used if an organization key is not passed as an attribute
+    - Facebook App Id will be used for facebook comments support
--- lib/Install.php
+++ lib/Install.php
@@ -4,53 +4,41 @@ class Install {
 
     function install() {
         $this->createPages();
-        $this->addRoles();
     }
 
+    /*
+     * This will create pages that you can then customize to fit your auctions' needs
+     */
     public function createPages()
     {
 
         // Insert the post into the database
         $pages = [
+            // Used for individual auction
+            'auction'      => [
+                'post_title'   => 'Auction',
+                'post_name'    => 'auction',
+                'post_content' => 'Auction',
+                'post_status'  => 'publish'
+            ],
+            // Used for auction list
+            'auctions'      => [
+                'post_title'   => 'Auctions',
+                'post_name'    => 'auctions',
+                'post_content' => 'Auctions',
+                'post_status'  => 'publish'
+            ],
+            // Used for viewing an auction item
             'auctionItem'        => [
                 'post_title'   => 'Auction Item',
                 'post_name'    => 'item',
                 'post_content' => 'Auction Item',
                 'post_status'  => 'publish'
             ],
-            'auctionDetail'      => [
-                'post_title'   => 'Auction',
-                'post_name'    => 'auction',
-                'post_content' => 'Auction',
-                'post_status'  => 'publish'
-            ]
         ];
-
-        // For Organization page
-//        'organizationDetail' => [
-//            'post_title'   => 'Organization Detail',
-//            'post_name'    => 'organization',
-//            'post_content' => 'Organization Detail.',
-//            'post_status'  => 'publish'
-//        ]
 
         foreach ($pages as $page) {
             wp_insert_post($page);
         }
-    }
-
-    function addRoles() {
-
-        add_role(
-            'handbid_bidder',
-            __('Bidder'),
-            [
-                'read'           => true, // true allows this capability
-                'edit_posts'     => false,
-                'delete_posts'   => false, // Use false to explicitly deny
-                'show_admin_bar' => false,
-                'level_0'        => true
-            ]
-        );
     }
 }
