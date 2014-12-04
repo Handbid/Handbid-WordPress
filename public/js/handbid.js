b4f37595e6f5106f7d03326e35a75d2e9c0d9bfd
Hemstreet <jonhemstreet@gmail.com>
2014-12-04 00:34:23 -0700

    hide auction error banner on refresh for firefox

--- public/js/handbid.js
+++ public/js/handbid.js
@@ -1,7 +1,15 @@
-(function($) {
-    $(document).ready(function() {
-       if($('[data-handbid-item-key], [data-no-bids], [data-tags]').length > 0) {
-           $('body').addClass('enable-handbid-fatal-error');
-       }
+(function ($) {
+    $(document).ready(function () {
+        if ($('[data-handbid-item-key], [data-no-bids], [data-tags]').length > 0) {
+            $('body').addClass('enable-handbid-fatal-error');
+        }
+
+        $(window).on('beforeunload', function () {
+
+            console.log('refreshing');
+            $('body').addClass('handbid-refreshing-page');
+
+        });
     });
+
 })(jQuery);
