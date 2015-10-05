95f9a659545e8212711f8482ad550de36ec8900c
unknown <bogdan.palijchuk@masterofcode.com>
2015-10-05 12:06:24 +0300

    auction details Google Map loading changes

--- public/js/details-map.js
+++ public/js/details-map.js
@@ -7,89 +7,79 @@
  * @author Master of Code (worldclass@masterofcode.com)
  */
 
-(function ($) {
+var smallMap;
+function auctionGoogleMapsInit(){
+    var addy = jQuery('.details-map').attr('data-address'),
+        key = 'AIzaSyCHLASKZkAlS4aX2JCcnR9XVqkrPpfdvvw',
+        small,
+        smallOptions = {scrollwheel: false, zoom: 12, disableDefaultUI: true},
+        large,
+        center,
+        geocoder,
+        largeOptions = {scrollwheel: false, zoom: 17, disableDefaultUI: true};
 
 
-    "use strict";
+    window.getDirections = function () {
 
-    /**
-     * Maps
-     */
-    $(document).ready(function () {
+        var myAddy = prompt('Enter your address.');
 
-        var addy = $('.details-map').attr('data-address'),
-            key = 'AIzaSyCHLASKZkAlS4aX2JCcnR9XVqkrPpfdvvw',
-            small,
-            smallOptions = {scrollwheel: false, zoom: 12, disableDefaultUI: true},
-            large,
-            center,
-            geocoder,
-            largeOptions = {scrollwheel: false, zoom: 17, disableDefaultUI: true};
+        if (myAddy) {
+            window.open('https://maps.google.com/maps?saddr=' + myAddy + '&daddr=' + addy);
+        }
+    };
 
+    if(typeof google.maps.Map == "function") {
 
-        // selects all nav items, and groups them to their respective content
-        $('.slider-nav [data-slider-nav-key="map"]').on('click', function () {
+        small = new google.maps.Map(jQuery('#small-map')[0], smallOptions);
+        large = new google.maps.Map(jQuery('#large-map')[0], largeOptions);
 
-            if (!small) {
+        if(typeof google.maps.Geocoder == "function") {
 
-                window.getDirections = function () {
+            geocoder = new google.maps.Geocoder();
+            geocoder.geocode({'address': addy}, function (results, status) {
 
-                    var myAddy = prompt('Enter your address.');
+                if (status === google.maps.GeocoderStatus.OK) {
 
-                    if (myAddy) {
-                        window.open('https://maps.google.com/maps?saddr=' + myAddy + '&daddr=' + addy);
-                    }
-                };
+                    center = results[0].geometry.location;
 
-                geocoder = new google.maps.Geocoder();
+                    small.setCenter(center);
+                    large.setCenter(center);
 
-                small = new google.maps.Map($('#small-map')[0], smallOptions);
-                large = new google.maps.Map($('#large-map')[0], largeOptions);
+                    new google.maps.Marker({
+                        map: small,
+                        icon: '/wp-content/themes/handbid/public/images/auction-details/small-pin.png',
+                        position: results[0].geometry.location
+                    });
 
-                geocoder.geocode({'address': addy}, function (results, status) {
+                    var largeMarker = new google.maps.Marker({
+                        map: large,
+                        icon: '/wp-content/themes/handbid/public/images/auction-details/large-pin.png',
+                        position: results[0].geometry.location
+                    });
 
-                    if (status === google.maps.GeocoderStatus.OK) {
+                    var infowindow = new google.maps.InfoWindow({
+                        content: jQuery('#map-marker-content').html()
+                    });
 
-                        center = results[0].geometry.location;
+                    google.maps.event.addListenerOnce(large, 'tilesloaded', function () {
+                        setTimeout(function () {
+                            infowindow.open(large, largeMarker);
+                        }, 1000);
+                    });
 
-                        small.setCenter(center);
-                        large.setCenter(center);
-
-                        new google.maps.Marker({
-                            map: small,
-                            icon: '/wp-content/themes/handbid/public/images/auction-details/small-pin.png',
-                            position: results[0].geometry.location
-                        });
-
-                        var largeMarker = new google.maps.Marker({
-                            map: large,
-                            icon: '/wp-content/themes/handbid/public/images/auction-details/large-pin.png',
-                            position: results[0].geometry.location
-                        });
-
-                        var infowindow = new google.maps.InfoWindow({
-                            content: $('#map-marker-content').html()
-                        });
-
-                        google.maps.event.addListenerOnce(large, 'tilesloaded', function () {
-                            setTimeout(function () {
-                                infowindow.open(large, largeMarker);
-                            }, 1000);
-                        });
-
-                    } else {
-                        alert("Geocode was not successful for the following reason: " + status);
-                    }
-                });
-
-
-            }
-
-
-        });
-
+                } else {
+                    alert("Geocode was not successful for the following reason: " + status);
+                }
+            });
+        }
+    }
+    smallMap = small;
+    jQuery('.slider-nav [data-slider-nav-key="map"]').on('click', function () {
+        setTimeout(function(){
+            google.maps.event.trigger(small, 'resize');
+            google.maps.event.trigger(large, 'resize');
+            small.setCenter(center);
+            large.setCenter(center);
+        }, 500);
     });
-
-
-
-}(jQuery));
+}
