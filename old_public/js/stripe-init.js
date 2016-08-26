cb23d156f640e3fea0e2b0a240e083363952b1ce
yana.tsylinska <yana.tsylinska@masterofcode.com>
2016-08-26 16:57:33 +0300

    Delete public_old

--- old_public/js/stripe-init.js
+++
@@ -1,9 +0,0 @@
-/**
- * @copyright Copyright (c) 2014-2015 Handbid Inc.
- * @category handbid
- * @package Handbid2-WordPress
- * @license Proprietary
- * @link http://www.handbid.com/
- * @author Master of Code (worldclass@masterofcode.com)
- */
-Stripe.setPublishableKey(stripePublishableKey);
